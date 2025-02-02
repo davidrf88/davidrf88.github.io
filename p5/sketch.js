2c9d01ec30979d9391741114cb1494d410b286d1
davidrf88 <davidrf88@gmail.com>
2025-02-02 16:17:53 -0500

    p5 js

---
+++ p5/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  let canvas = createCanvas(720, 1000); // Set canvas size
+  canvas.parent('canvas-container');  // Attach to the div
+}
+
+function draw() {
+  background(120);
+}
